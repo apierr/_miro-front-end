cd581129246742e0fc8a25c9f7652da5453b7b00
apierro <antonio.pierro@gmail.com>
2019-07-17 17:20:29 +0200

    first commit of sheetReader class

---
+++ src/sheetReader.js
@@ -0,0 +1,27 @@
+class SheetReader {
+    constructor(global_wb) {
+      this.sheet = global_wb.Sheets.Sheet1
+      console.log(this.getColumnName());
+      console.log(this.getPointsCount());
+    }
+
+    getRef() {
+      return global_wb.Sheets.Sheet1["!ref"];
+    }
+
+    getUppercaseLetters() {
+      return [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    }
+
+    getColumnName() {
+      // TODO find a more compact way to do it
+      const lastColumn = this.getRef().match(/[A-E]/g)[1];
+      const uppercaseLetters = this.getUppercaseLetters()
+      const lastIndex = uppercaseLetters.indexOf(lastColumn) + 1;
+      return uppercaseLetters.slice(0, lastIndex);
+    }
+
+    getPointsCount() {
+      return this.getRef().match((/\d/g))[1]-1;
+    }
+  }
