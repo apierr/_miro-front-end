a4dc7a82c816b7254ed538921fd459c3be964c51
apierro <antonio.pierro@gmail.com>
2019-07-17 17:49:19 +0200

    Added PlotWriter

--- src/plotWriter.js
+++ src/plotWriter.js
@@ -1,26 +1,35 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+class PlotWriter {
+    constructor() {
+        this.ctx = document.getElementById('myChart').getContext('2d');
+        this.setChart();
+    }
+    setChart() {
+        new Chart(this.ctx, {
+            // The type of chart we want to create
+            type: 'line',
+        
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Dataset 1',
+                    borderWidth: 1,
+                    data: [
+                        14, 24, 23, 23, 12, 12, 5
+                    ]
+                }, {
+                    label: 'Dataset 2',
+                    borderWidth: 1,
+                    data: [
+                        10,23, 23, 13, 22, 12, 12
+                    ]
+                }]
+            },
+        
+            // Configuration options go here
+            options: {}
+        });
+    }
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Dataset 1',
-            borderWidth: 1,
-            data: [
-                14, 24, 23, 23, 12, 12, 5
-            ]
-        }, {
-            label: 'Dataset 2',
-            borderWidth: 1,
-            data: [
-                10,23, 23, 13, 22, 12, 12
-            ]
-        }]
-    },
+}
 
-    // Configuration options go here
-    options: {}
-});
