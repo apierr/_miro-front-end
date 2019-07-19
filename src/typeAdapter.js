3bc51968a769e2f2afd648cc1e419f90d67332d1
apierro <antonio.pierro@gmail.com>
2019-07-19 07:22:08 +0200

    refactor getDfType in typeAdapter class

--- src/typeAdapter.js
+++ src/typeAdapter.js
@@ -5,14 +5,16 @@ class TypeAdapter {
 
     getDfType() {
         var dfType = this.dfType;
-        dfType = dfType.replace(/s/gi, 'C')
-            .replace(/n/gi, 'N')
-            .replace(/d/gi, 'D');
-
-        const pointCount = dfType.match((/(\d)P/))[1];
-        const pointRange = [3, 5, 8, 13, 21, 100, 1000, 10000];
         var pointLimit;
-        pointRange.some((e,i) => {if(e>pointCount) {pointLimit = i; return true;}});
+        const pointCount = dfType.match((/(\d)P/))[1];
+        const pointRange = [4, 10, 99, 1000, Infinity];
+
+        pointRange.some((e,i) => {
+            if(e>pointCount) {pointLimit = i + 1; return true;}
+        });
+        dfType = dfType.replace(/s/gi, 'C')
+        .replace(/n/gi, 'N')
+        .replace(/d/gi, 'D');
         dfType = dfType.replace(/(\d{1,})P/,pointLimit+'P');
 
         return dfType;
