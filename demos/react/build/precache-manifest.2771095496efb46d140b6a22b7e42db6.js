cc22a3f24c7c498f58d1849d51ad1a62dcf448ce
apierro <antonio.pierro@gmail.com>
2019-07-22 17:35:22 +0200

    added package.json

---
+++ demos/react/build/precache-manifest.2771095496efb46d140b6a22b7e42db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741efb5e1ca748d276fdbe4fc20d24be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1405d1f4bf0171322f0c",
+    "url": "/static/css/main.e9f744fa.chunk.css"
+  },
+  {
+    "revision": "f52f95e2189089e76861",
+    "url": "/static/js/2.73ea9450.chunk.js"
+  },
+  {
+    "revision": "1405d1f4bf0171322f0c",
+    "url": "/static/js/main.4017024e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
