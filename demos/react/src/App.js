b8241b920929c2539e9cb98b10424d1c9af7e6c8
apierro <antonio.pierro@gmail.com>
2019-07-23 14:05:29 +0200

    remove useless files

--- demos/react/src/App.js
+++ demos/react/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SheetJSApp from './sheetjs';
 
 
 function App() {
-  
   return (
-    <div className="App">
-      hello
-    </div>
+       <SheetJSApp />
   );
 }
 
